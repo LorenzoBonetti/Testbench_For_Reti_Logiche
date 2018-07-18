baabdd46e21045ad88750eaf4d7d434d85890311
Lorenzo Bonetti <37806046+LorenzoBonetti@users.noreply.github.com>
2018-07-18 20:49:54 +0200

    add tb from 1 to 3

---
+++ Test Singoli/testbench_1.vhd
@@ -0,0 +1,95 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+ 
+entity project_tb is
+end project_tb;
+
+
+architecture projecttb of project_tb is
+constant c_CLOCK_PERIOD		: time := 15 ns;
+signal   tb_done		: std_logic;
+signal   mem_address		: std_logic_vector (15 downto 0) := (others => '0');
+signal   tb_rst		    : std_logic := '0';
+signal   tb_start		: std_logic := '0';
+signal   tb_clk		    : std_logic := '0';
+signal   mem_o_data,mem_i_data		: std_logic_vector (7 downto 0);
+signal   enable_wire  		: std_logic;
+signal   mem_we		: std_logic;
+
+type ram_type is array (65535 downto 0) of std_logic_vector(7 downto 0);
+signal RAM:ram_type:=(2 => "00011000", 3 =>"00000111",4 => "00000000", 30 => "00000011", 31 => "00000011", 32 => "00000011", 33 => "00000011", 36 => "00000111", 37 => "00000111", 38 => "00000111", 39 => "00000111", 42 => "00001011", 43 => "00001011", 44 => "00001011", 45 => "00001011", 48 => "00001111", 54 => "00000011", 60 => "00000111", 66 => "00011111", 72 => "00011001", 78 => "00000011", 79 => "00000011", 80 => "00000011", 84 => "00000111", 85 => "00000111", 86 => "00000111", 90 => "00011111", 91 => "00011111", 92 => "00011111", 96 => "00011001", 102 => "00000011", 108 => "00000111", 114 => "00011111", 120 => "00011001", 126 => "00000011", 132 => "00000111", 133 => "00000111", 134 => "00000111", 135 => "00000111", 138 => "00001011", 139 => "00001011", 140 => "00001011", 141 => "00001011", 144 => "00001111", 145 => "00001111", 146 => "00001111", 147 => "00001111", others => (others =>'0'));
+component project_reti_logiche is 
+    port (
+            i_clk         : in  std_logic;
+            i_start       : in  std_logic;
+            i_rst         : in  std_logic;
+            i_data       : in  std_logic_vector(7 downto 0); --1 byte
+            o_address     : out std_logic_vector(15 downto 0); --16 bit addr: max size is 255*255 + 3 more for max x and y and thresh.
+            o_done            : out std_logic;
+            o_en         : out std_logic;
+            o_we       : out std_logic;
+            o_data            : out std_logic_vector (7 downto 0)
+          );
+end component project_reti_logiche;
+
+
+begin 
+	UUT: project_reti_logiche
+	port map (
+		  i_clk      	=> tb_clk,	
+          i_start       => tb_start,
+          i_rst      	=> tb_rst,
+          i_data    	=> mem_o_data,
+          o_address  	=> mem_address, 
+          o_done      	=> tb_done,
+          o_en   	=> enable_wire,
+		  o_we 	=> mem_we,
+          o_data    => mem_i_data
+);
+
+p_CLK_GEN : process is
+  begin
+    wait for c_CLOCK_PERIOD/2;
+    tb_clk <= not tb_clk;
+  end process p_CLK_GEN; 
+  
+  
+MEM : process(tb_clk)
+   begin
+    if tb_clk'event and tb_clk = '1' then
+     if enable_wire = '1' then
+      if mem_we = '1' then
+       RAM(conv_integer(mem_address))              <= mem_i_data;
+       mem_o_data                      <= mem_i_data after 1 ns;
+      else
+       mem_o_data <= RAM(conv_integer(mem_address)) after 1 ns;
+      end if;
+     end if;
+    end if;
+   end process;
+test : process is
+begin 
+wait for 100 ns;
+wait for c_CLOCK_PERIOD;
+tb_rst <= '1';
+wait for c_CLOCK_PERIOD;
+tb_rst <= '0';
+wait for c_CLOCK_PERIOD;
+tb_start <= '1';
+wait for c_CLOCK_PERIOD; 
+tb_start <= '0';
+wait until tb_done = '1';
+wait until tb_done = '0';
+wait until rising_edge(tb_clk); 
+
+ assert RAM(1) = "00000000" report "FAIL high bits" severity failure;
+ assert RAM(0) = "10101000" report "FAIL low bits" severity failure;
+
+
+
+assert false report "Simulation Ended!, test passed" severity failure;
+end process test;
+
+end projecttb; 
